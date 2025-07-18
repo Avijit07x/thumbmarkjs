7ec61a6b433d20f403bbd375c2dfbe75c64fea35
Avijit07x <deyavijit134@gmail.com>
2025-07-19 00:05:30 +0530

    fix : ip

--- src/components/IpTrack.tsx
+++ src/components/IpTrack.tsx
@@ -3,10 +3,17 @@ import { useEffect, useState } from "react";
 const IpTrack: React.FC = () => {
 	const [ip, setIp] = useState<string>("");
 	useEffect(() => {
-		fetch("https://api.ipify.org/?format=json")
-			.then((res) => res.json())
-			.then((data) => setIp(data.ip))
-			.catch((e: any) => setIp("Error fetching IP"));
+		const fetchIp = async () => {
+			try {
+				const res = await fetch("https://api.ipify.org/?format=json");
+				const data = await res.json();
+				setIp(data.ip);
+				console.log("ip :", data.ip);
+			} catch (error: any) {
+				console.log(error);
+			}
+		};
+		fetchIp();
 	}, []);
 
 	return (
